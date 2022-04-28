daf5a464d57291ec2b0ef96af7ccacc09a498056
shiina <79903295@qq.com>
2022-04-28 15:44:50 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@RequestMapping("main")
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @RequestMapping(value = "test" ,method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        return "123";
+    }
 }
